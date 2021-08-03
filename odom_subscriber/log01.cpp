b93c931711266b339ac30948f093a43a33632d16
KomKGT <81642936+KomKGT@users.noreply.github.com>
2021-08-03 13:18:21 +0700

    Add files via upload

---
+++ odom_subscriber/log01.cpp
@@ -0,0 +1,69 @@
+#include <nav_msgs/Odometry.h> // Needed for accessing Odometry data
+#include <ros/ros.h>           // Needed for creating the node, etc
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <time.h>
+using namespace std;
+
+int NumberOfDataTruth = 1;  
+int NumberOfDataOdom = 1;
+// This functions get called when a new odometry message gets to the subscriber
+// It automatically gets the odometry message as a parameter
+// It prints out various parts of the message
+void counterCallback(const nav_msgs::Odometry::ConstPtr &msg) //Write Truth Path from GAZEBO
+{
+  // ROS_INFO("%s", msg->header.frame_id.c_str());
+  // ROS_INFO("%f", msg->twist.twist.linear.x);
+  //ROS_INFO("%f", msg->pose.pose.position.x);
+  char buff[100];
+  time_t now = time (0);
+  strftime (buff, 100, "%Y%m%d %H:%M:%S", localtime (&now));	//YYYYMMDD HH:MM:SS 
+  
+  
+  ofstream writefile("/home/SharedFile/truth01.csv",ios::app);
+  //writefile.open("/home/SharedFile/log01.csv");
+  //writefile<<"x"<<","<<"y"<<"\n";
+  
+  writefile<<NumberOfDataTruth<<","<<buff<<","<<msg->pose.pose.position.x<<","<<msg->pose.pose.position.y<<endl;
+  NumberOfDataTruth++;
+  cout<<"Collecting Truth Robot01 Data ...."<<endl;
+  //cout<<msg->pose.pose.position.x<<endl;
+  //cout<<msg->pose.pose.position.y<<endl;
+}
+void counterCallback2(const nav_msgs::Odometry::ConstPtr &msg) //Write Odometry From Robot01
+{
+  // ROS_INFO("%s", msg->header.frame_id.c_str());
+  // ROS_INFO("%f", msg->twist.twist.linear.x);
+  //ROS_INFO("%f", msg->pose.pose.position.x);
+  char buff[100];
+  time_t now = time (0);
+  strftime (buff, 100, "%Y%m%d %H:%M:%S", localtime (&now));      //YYYYMMDD HH:MM:SS 
+  
+  
+  ofstream writefile("/home/SharedFile/log01.csv",ios::app);
+  //writefile.open("/home/SharedFile/log01.csv");
+  //writefile<<"x"<<","<<"y"<<"\n";
+  
+  writefile<<NumberOfDataOdom<<","<<buff<<","<<msg->pose.pose.position.x<<","<<msg->pose.pose.position.y<<endl;
+  NumberOfDataOdom++;
+  cout<<"Collecting Odom Robot01 Data ...."<<endl;
+  //cout<<msg->pose.pose.position.x<<endl;
+  //cout<<msg->pose.pose.position.y<<endl;
+}
+
+int main(int argc, char **argv) {
+  // Create a node to run the code
+  ros::init(argc, argv, "log01&truth01");
+  ros::NodeHandle nh;
+  
+
+  // create a subscriber to the "/odom" topic so we can get the odometry message
+  ros::Subscriber sub = nh.subscribe("/tb3_0/ground_truth/state", 1000, counterCallback);
+  ros::Subscriber sub2 = nh.subscribe("/tb3_0/odom", 1000, counterCallback2);
+  
+  // Run program until manually stopped
+  ros::spin();
+
+  return 0;
+}
---
+++ odom_subscriber/odom_sub_robot01.cpp
@@ -0,0 +1,37 @@
+#include <nav_msgs/Odometry.h> // Needed for accessing Odometry data
+#include <ros/ros.h>           // Needed for creating the node, etc
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+// This functions get called when a new odometry message gets to the subscriber
+// It automatically gets the odometry message as a parameter
+// It prints out various parts of the message
+void counterCallback(const nav_msgs::Odometry::ConstPtr &msg) {
+  // ROS_INFO("%s", msg->header.frame_id.c_str());
+  // ROS_INFO("%f", msg->twist.twist.linear.x);
+  //ROS_INFO("%f", msg->pose.pose.position.x);
+  ofstream writefile;
+  writefile.open("/home/SharedFile/rfid01.csv");
+  writefile<<"x"<<","<<"y"<<"\n";
+  writefile<<msg->pose.pose.position.x<<","<<msg->pose.pose.position.y;
+  writefile.close();
+  cout<<"Collecting Robot01 Data ...."<<endl;
+  //cout<<msg->pose.pose.position.x<<endl;
+  //cout<<msg->pose.pose.position.y<<endl;
+}
+
+int main(int argc, char **argv) {
+  // Create a node to run the code
+  ros::init(argc, argv, "odom_sub_node01");
+  ros::NodeHandle nh;
+
+  // create a subscriber to the "/odom" topic so we can get the odometry message
+  ros::Subscriber sub = nh.subscribe("/tb3_0/odom", 1000, counterCallback);
+  
+  
+  // Run program until manually stopped
+  ros::spin();
+
+  return 0;
+}
